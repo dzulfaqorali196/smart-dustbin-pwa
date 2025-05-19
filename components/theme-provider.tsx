c288a6e43af2656478ad2e351b434d1cce9ad1cb
Dzul <whysoserius30@gmail.com>
2025-05-19 20:28:05 +0700

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 export function ThemeProvider({ 
   children,
   ...props
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 }: React.PropsWithChildren<any>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 } 
