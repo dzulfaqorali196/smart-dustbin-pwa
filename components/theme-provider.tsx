8e15050ee3cfd4e7ee08618027f855933bbd10c6
Dzul <whysoserius30@gmail.com>
2025-05-19 20:24:28 +0700

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,19 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+
+// Definisikan tipe secara langsung untuk menghindari error import
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: string;
+  storageKey?: string;
+  themes?: string[];
+  enableSystem?: boolean;
+  enableColorScheme?: boolean;
+  disableTransitionOnChange?: boolean;
+  forcedTheme?: string;
+  attribute?: string;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
