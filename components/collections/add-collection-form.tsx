aec774a1f9b88152216c19f961c5558b5183dde3
Dzul <whysoserius30@gmail.com>
2025-05-21 01:48:54 +0700

    Update add-collection-form.tsx

--- components/collections/add-collection-form.tsx
+++ components/collections/add-collection-form.tsx
@@ -8,9 +8,8 @@ import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { createCollection } from '@/lib/api/collections';
 import { FormattedBin } from '@/lib/api/bins';
 
